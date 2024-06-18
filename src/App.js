a34a0512833c241c291a95a67b2b61665451e369
김동욱 <email@example.com>
2024-06-18 14:09:15 +0900

    useState use new list make

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  /*useEffect(function () {
-    console.log("hi :");
-    return () => console.log("bye :(");
-  }, []);*/
-  useEffect(() => {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); // 현재값을 받아오는 currentArray 함수를 만들고 입력한 toDo와 현재 배열에 있는 값을 더 한다. setToDos는 toDos있는 값을 기억하는 함수네
+    setToDo("");
+  };
+  console.log(toDos);
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}> {showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
-
-//딱 한번만 실행되고 다시는 실행되지 않도록 하고 싶을 수있는 방법 : useEffect //
