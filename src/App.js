ac49ca506eb8323df80431a1b64b8d509868ae47
김동욱 <email@example.com>
2024-06-18 11:27:36 +0900

    useEffect_cleanup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import { useEffect, useState } from "react";
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  useEffect(() => {
-    console.log({ keyword });
-  }, [keyword]);
 
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword & counter' changes");
-  }, [keyword, counter]);
+function Hello() {
+  function hiFn() {
+    console.log("created :)");
+    return byFn; // 처음 show 할 때 created가 나오고 컴포넌트가 파괴될 때 리턴한 Function을 실행한다.
+  }
+  function byFn() {
+    console.log("bye :(");
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}> {showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
 
 export default App;
 
-//딱 한번만 실행되고 다시는 실행되지 않도록 하고 싶을 수있는 방법 : useEffect
+//딱 한번만 실행되고 다시는 실행되지 않도록 하고 싶을 수있는 방법 : useEffect //
