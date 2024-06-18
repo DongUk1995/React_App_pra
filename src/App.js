8aeb3a16ab237630718b82717b24acdeb3d9eba9
김동욱 <email@example.com>
2024-06-18 10:49:51 +0900

    useEffect 기능 연습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import Button from "./button";
-import styles from "./app.module.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}> Welcome Back</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
 
 export default App;
+
+//딱 한번만 실행되고 다시는 실행되지 않도록 하고 싶을 수있는 방법 : useEffect
